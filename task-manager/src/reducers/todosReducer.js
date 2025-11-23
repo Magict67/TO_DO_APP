a9bec6af2762c468c6a71e6da8492e1781a5953d
magict67 <christopheradonaithomas@gmail.com>
2025-11-23 10:00:31 -0800

    build todosreducer

---
+++ task-manager/src/reducers/todosReducer.js
@@ -0,0 +1,6 @@
+// beginning tasks
+const initialState = [
+  { id: 1, text: 'Create wireframes', completed: true },
+  { id: 2, text: 'Set up Redux store', completed: false },
+  { id: 3, text: 'Build Contact Form', completed: false }
+];
