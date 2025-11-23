a206d06e110f679b1c48a8ab993a6a4ffa3c6b3f
magict67 <christopheradonaithomas@gmail.com>
2025-11-23 09:49:10 -0800

    add basic appjs ready code

--- task-manager/src/App.js
+++ task-manager/src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>Community Task Manager</h1>
+      <p>Welcome! We are ready to start building.</p>
+    </div>
+  );
+}
+
+export default App;
