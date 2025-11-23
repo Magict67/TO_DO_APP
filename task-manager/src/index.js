1f7cd1d043ee47cea52e61469bf898ec7a15a28a
magict67 <christopheradonaithomas@gmail.com>
2025-11-23 11:51:14 -0800

    connect store shrome with devtools provider access store

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,3 +5,17 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 import './index.css';
 import App from './App';
+//Redux Store with Main Manager
+const store = createStore(
+  rootReducer,
+  //use chrome with Redux DevTools if installed
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // 2. App in the Provider to access the Store
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
