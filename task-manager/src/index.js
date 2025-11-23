6eb853bbd42fa8851f14dd1c2f68ce737864bfe2
magict67 <christopheradonaithomas@gmail.com>
2025-11-23 11:42:15 -0800

    store import instructions

---
+++ task-manager/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import './index.css';
+import App from './App';
