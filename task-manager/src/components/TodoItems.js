0f6fc68693e3281cc6222f5cbfc2385240f5d2ce
magict67 <christopheradonaithomas@gmail.com>
2025-11-23 20:18:46 -0800

    same

--- task-manager/src/components/TodoItems.js
+++ task-manager/src/components/TodoItems.js
@@ -1,3 +1,13 @@
 import React from 'react';
 
 //  receive 'text' and 'completed'parent
+const TodoItem = ({ text, completed }) => {
+  return (
+    <li style={{
+      textDecoration: completed ? 'line-through' : 'none',
+      padding: '10px',
+      borderBottom: '1px solid #eee'
+    }}>
+    </li>
+  );
+};
