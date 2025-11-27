9e0ba731eae1f8149dd1474ce34a1c8765264952
magict67 <christopheradonaithomas@gmail.com>
2025-11-27 14:53:47 -0800

    Task list look

---
+++ task-manager/src/views/TodosView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodosView = () => {
+  return (
+    <div className="todos-view">
+      <h2>Task List (Todos)</h2>
+      <p>This is where the todo list functionality will go.</p>
+    </div>
+  );
+};
+
+export default TodosView;
