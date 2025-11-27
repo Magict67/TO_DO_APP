0c41b0974cc06bfa51b4d7d8b5a512025ea952e1
magict67 <christopheradonaithomas@gmail.com>
2025-11-27 14:58:35 -0800

    Communiy view

---
+++ task-manager/src/views/ContactView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactView = () => {
+  return (
+    <div className="contact-view">
+      <h2>Contact the Community</h2>
+      <p>This is where the controlled contact form will go.</p>
+    </div>
+  );
+};
+
+export default ContactView;
