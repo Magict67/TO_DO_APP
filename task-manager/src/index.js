77df06bb50d80ce9826fba7a6ee76c4cae6abe1c
magict67 <christopheradonaithomas@gmail.com>
2025-12-09 19:17:15 -0800

    index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers';
 import './index.css';
 import App from './App';
-//Redux Store with Main Manager
-const store = createStore(
-  rootReducer,
-  //use chrome with Redux DevTools if installed
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'; // 💡 Import the Provider component
+import store from './store'; // 💡 Import the Redux store
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  // 2. App in the Provider to access the Store
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    {/* Wrap the entire application in the Redux Provider */}
+    <Provider store={store}> 
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
+
+reportWebVitals();
