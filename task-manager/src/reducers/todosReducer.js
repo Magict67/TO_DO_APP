2f8cdac1d97bc130e046c7e021089e7a91d57ec2
magict67 <christopheradonaithomas@gmail.com>
2025-12-09 19:13:35 -0800

    reducers,js

--- task-manager/src/reducers/todosReducer.js
+++ task-manager/src/reducers/todosReducer.js
@@ -1,16 +1,36 @@
-// beginning tasks
-const initialState = [
-  { id: 1, text: 'Create wireframes', completed: true },
-  { id: 2, text: 'Set up Redux store', completed: false },
-  { id: 3, text: 'Build Contact Form', completed: false }
-];
-// data handler
-const todosReducer = (state = initialState, action) => {
+import { ADD_TODO, TOGGLE_TODO, DELETE_TODO } from '../actions/todoActions';
+
+const initialState = {
+  todos: [], // The array that will hold all your tasks
+};
+
+const todoReducer = (state = initialState, action) => {
   switch (action.type) {
-    // return created list
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload], // Add new task to the array
+      };
+
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map(todo =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed } // Flip the completed status
+            : todo
+        ),
+      };
+
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.payload), // Remove task by ID
+      };
+
     default:
       return state;
   }
 };
 
-export default todosReducer;
+export default todoReducer;
