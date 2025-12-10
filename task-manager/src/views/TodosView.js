d916473396ac6caca0ecd75401eb5d2dbee9077c
magict67 <christopheradonaithomas@gmail.com>
2025-12-09 19:50:30 -0800

    Todosview

--- task-manager/src/views/TodosView.js
+++ task-manager/src/views/TodosView.js
@@ -1,10 +1,18 @@
 import React from 'react';
+// 💡 FIX: Import the new TodoInput component
+import TodoInput from '../components/TodoInput'; 
+// We will add the TodoList component here in a later step
 
 const TodosView = () => {
   return (
-    <div className="todos-view">
+    <div style={{ padding: '20px' }}>
       <h2>Task List (Todos)</h2>
       <p>This is where the todo list functionality will go.</p>
+
+      {/* 💡 FIX: Render the TodoInput Component */}
+      <TodoInput /> 
+
+      {/* TodoList will be rendered here later */}
     </div>
   );
 };
