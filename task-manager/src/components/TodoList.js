06da3d5da012db8a351735fd134d461df98eb796
magict67 <christopheradonaithomas@gmail.com>
2025-12-10 20:36:59 -0800

    update TLst.js

--- task-manager/src/components/TodoList.js
+++ task-manager/src/components/TodoList.js
@@ -1,28 +1,29 @@
 import React from 'react';
-// import TodoItem component later.
-// this is list holder.
+import { useSelector } from 'react-redux'; // 💡 NEW: Hook to read state from Redux
+import TodoItem from './TodoItem'; // 💡 NEW: Import the item component
 
-const TodoList = ({ todos }) => {
-  // receives list of tasks
-  // called `todos`from parent container
+const TodoList = () => {
+  // Use useSelector to read the 'todos' array from the Redux store
+  // We set the state slice name to 'tasks' in rootReducer (state.tasks.todos)
+  const todos = useSelector(state => state.tasks.todos); 
 
-  if (!todos || todos.length === 0) {
-    // If empty, show message
-    return <p>Hooray! No tasks to show. You can add one below!</p>;
-  }
+  // If the list is empty, show a congratulatory message
+  if (todos.length === 0) {
+    return (
+      <p style={{ marginTop: '30px', fontSize: '1.2em', color: '#555' }}>
+        Hooray! No tasks to show. You can add one below!
+      </p>
+    );
+  }
 
-  // If tasks, loop through to display them.
-  return (
-    <ul style={{ listStyleType: 'none', padding: 0 }}>
-      {/* Map over 'todos' array
-        make <TodoItem /> for each! 
-      */}
-      {/* check if list is receiving data.
-        See "Displaying 3 tasks" in browser, it's working!
-      */}
-      <p>Displaying {todos.length} tasks...</p>
-    </ul>
-  );
+  return (
+    <ul style={{ listStyle: 'none', padding: 0, width: '500px', margin: '20px auto', textAlign: 'left' }}>
+      {/* Map over the tasks and render a TodoItem for each one */}
+      {todos.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
 };
 
 export default TodoList;
