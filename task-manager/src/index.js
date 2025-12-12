9cecae1832d516c6a00abdcb596076a40316d777
magict67 <christopheradonaithomas@gmail.com>
2025-12-12 06:50:01 -0800

    add provider store

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'; // 💡 Import the Provider component
-import store from './store'; // 💡 Import the Redux store
+import { Provider } from 'react-redux'; // Import Provider component
+import store from './store'; // Import Redux store
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {/* Wrap the entire application in the Redux Provider */}
+    {/* Wrap app in Redux Provider */}
     <Provider store={store}> 
       <App />
     </Provider>
