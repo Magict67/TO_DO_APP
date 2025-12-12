91f19d1d72e32816406bf19c1dccfbcab758bd0e
magict67 <christopheradonaithomas@gmail.com>
2025-12-12 07:49:53 -0800

    what? state slice name?

--- task-manager/src/components/TodoList.js
+++ task-manager/src/components/TodoList.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux'; // 💡 NEW: Hook to read state from Redux
-import TodoItem from './TodoItem'; // 💡 NEW: Import the item component
+import { useSelector } from 'react-redux'; // Hook to read state from Redux
+import TodoItem from './TodoItem'; // Import the item component
 
 const TodoList = () => {
-  // Use useSelector to read the 'todos' array from the Redux store
-  // We set the state slice name to 'tasks' in rootReducer (state.tasks.todos)
+  // useSelector reads 'todos' array from Redux store
+  // set state slice name to 'tasks' in rootReducer (state.tasks.todos)
   const todos = useSelector(state => state.tasks.todos); 
 
-  // If the list is empty, show a congratulatory message
+  // If list empty, show silly message
   if (todos.length === 0) {
     return (
       <p style={{ marginTop: '30px', fontSize: '1.2em', color: '#555' }}>
-        Hooray! No tasks to show. You can add one below!
+        Yippie! No tasks! Mr. Snuffleupagus has left the building! Add another below
       </p>
     );
   }
