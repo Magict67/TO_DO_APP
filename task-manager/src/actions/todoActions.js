9d02c394f805a85f7adfa711c614553edc71bf8a
magict67 <christopheradonaithomas@gmail.com>
2025-12-20 20:31:27 -0800

    Actions  for vis

--- task-manager/src/actions/todoActions.js
+++ task-manager/src/actions/todoActions.js
@@ -1,26 +1,31 @@
-// Define unique action types
+// unique action types
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
-
-// Action Creator for adding a new task
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+// Action Creator add new task
 export const addTodo = (text) => ({
   type: ADD_TODO,
   payload: {
-    id: Date.now(), // Use current timestamp for a unique ID
+    id: Date.now(), // current timestamp for a unique ID
     text,
     completed: false,
   },
 });
 
-// Action Creator for toggling a task's completion status
+// toggle task completion status
 export const toggleTodo = (id) => ({
   type: TOGGLE_TODO,
   payload: id,
 });
 
-// Action Creator for deleting a task
+// AC for delete task
 export const deleteTodo = (id) => ({
   type: DELETE_TODO,
   payload: id,
+});
+
+export const setVisibilityFilter = (filter) => ({
+  type: SET_VISIBILITY_FILTER,
+  payload: filter,
 });
